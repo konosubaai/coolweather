25db6fe28d57fbe2591a2e8e8c4596991cc981ce
konosubaai <2297879021@qq.com>
2019-08-08 16:39:28 +0800

    增加后台自动更新天气功能及修改图标和名称。

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -210,6 +212,12 @@ public class WeatherActivity extends AppCompatActivity {
             maxText.setText(forecast.temperature.max);
             minText.setText(forecast.temperature.min);
             forecastLayout.addView(view);
+        }
+        if (weather!=null && "ok".equals(weather.status)){
+            Intent intent = new Intent(this,AutoUpdateService.class);
+            startService(intent);
+        }else {
+            Toast.makeText(this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
         }
         if (weather.aqi!=null){
             aqiText.setText(weather.aqi.city.aqi);
